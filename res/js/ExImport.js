a3c63e8b29328a77be62714918c5667de0333394
epicX <souravgain605@gmail.com>
2020-07-16 21:35:45 +0530

    Clear cards before importing

--- res/js/ExImport.js
+++ res/js/ExImport.js
@@ -10,49 +10,39 @@ function readFile(event){
                 return;
             }
             verifyObj(obj);
-
-            console.log("Executing");
         }
     }
     FILE.readAsText(event.target.files[0]);
 }
 
-function verifyObj(obj){
-    var count=0;
-
+async function verifyObj(obj){
+    cleanCards();
+    await sleep(200);
     for(var i=0; i<obj.length;i++){
         if(obj[i].compoType=="cpu"){
             IMPORT[0]=obj[i];
             load("cpuList","Dummy","Dummy",IMPORT[0].brand,IMPORT[0].model,IMPORT[0].price,false);
-            count++;
         }else if(obj[i].compoType=="motherboard"){
             IMPORT[1]=obj[i];
             load("motherboardList","Dummy","Dummy",IMPORT[1].brand,IMPORT[1].model,IMPORT[1].price,false);
-            count++;
         }else if(obj[i].compoType=="ram"){
             IMPORT[2]=obj[i];
             load("ramList","Dummy","Dummy",IMPORT[2].brand,IMPORT[2].model,IMPORT[2].price,false);
-            count++;
         }else if(obj[i].compoType=="gpu"){
             IMPORT[3]=obj[i];
             load("gpuList","Dummy","Dummy",IMPORT[3].brand,IMPORT[3].model,IMPORT[3].price,false);
-            count++;
         }else if(obj[i].compoType=="hdd"){
             IMPORT[4]=obj[i];
             load("hddList","Dummy","Dummy",IMPORT[4].brand,IMPORT[4].model,IMPORT[4].price,false);
-            count++;
         }else if(obj[i].compoType=="ssd"){
             IMPORT[5]=obj[i];
             load("ssdList","Dummy","Dummy",IMPORT[5].brand,IMPORT[5].model,IMPORT[5].price,false);
-            count++;
         }else if(obj[i].compoType=="psu"){
             IMPORT[6]=obj[i];
             load("psuList","Dummy","Dummy",IMPORT[6].brand,IMPORT[6].model,IMPORT[6].price,false);
-            count++;
         }else if(obj[i].compoType=="case"){
             IMPORT[7]=obj[i];
             load("caseList","Dummy","Dummy",IMPORT[7].brand,IMPORT[7].model,IMPORT[7].price,false);
-            count++;
         }
     }
 }
@@ -67,7 +57,7 @@ function exportJson(){
     document.getElementById("exportBtn").setAttribute("href",x);
 }
 
-function cleanCards(){
+async function cleanCards(){
     clearData('cpu','cpu_clearBtnPanel');
     clearData('motherboard','motherboard_clearBtnPanel');
     clearData('ram','ram_clearBtnPanel');
